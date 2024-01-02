b03f54d9d9d430cefe6c3281a69d064a9093ea7d
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2024-01-01 21:03:07 -0500

    Update action_button_apply.R

--- dependencies/action_button_apply.R
+++ dependencies/action_button_apply.R
@@ -1,14 +1,14 @@
 
 ob_func_apply <- function(input, output, session, json){
-  func_name <- input$func_name
+  func_name <- retrieve(input$func_name)
   if (is.null(func_name)){
     return()
   }
-  json$FunctionList[[func_name]]$FunctionName <- input$func_act
-  json$FunctionList[[func_name]]$FaaSServer <- input$func_faas
+  json$FunctionList[[func_name]]$FunctionName <- retrieve(input$func_act)
+  json$FunctionList[[func_name]]$FaaSServer <- retrieve(input$func_faas)
   json$FunctionList[[func_name]]$Arguments <- retrieve(input$func_args)
   json$FunctionList[[func_name]]$InvokeNext <- retrieve(input$func_next)
-  json$ActionContainers[[func_name]] <- input$func_container
+  json$ActionContainers[[func_name]] <- retrieve(input$func_container)
   json$FunctionCRANPackage[[input$func_act]] <- retrieve(input$func_cran_repo)
   json$FunctionGitHubPackage[[input$func_act]] <-retrieve(input$func_gh_package)
   json$FunctionGitRepo[[input$func_act]] <- retrieve(input$func_gh_repo)
@@ -20,24 +20,24 @@ ob_func_apply <- function(input, output, session, json){
 }
 
 ob_faas_apply <- function(input, output, session, json){
-  faas_name <- input$faas_name
+  faas_name <- retrieve(input$faas_name)
   if (is.null(faas_name)){
     return()
   }
-  json$ComputeServers[[faas_name]]$FaaSType <- input$faas_type
+  json$ComputeServers[[faas_name]]$FaaSType <- retrieve(input$faas_type)
   switch(json$ComputeServers[[faas_name]]$FaaSType,
          "GitHubActions"={
-           json$ComputeServers[[faas_name]]$UserName <- input$faas_gh_user
-           json$ComputeServers[[faas_name]]$ActionRepoName <- input$faas_gh_repo
-           json$ComputeServers[[faas_name]]$Branch <- input$faas_gh_ref
+           json$ComputeServers[[faas_name]]$UserName <- retrieve(input$faas_gh_user)
+           json$ComputeServers[[faas_name]]$ActionRepoName <- retrieve(input$faas_gh_repo)
+           json$ComputeServers[[faas_name]]$Branch <- retrieve(input$faas_gh_ref)
          },
          "Lambda"={
-           json$ComputeServers[[faas_name]]$Region <- input$faas_ld_region
+           json$ComputeServers[[faas_name]]$Region <- retrieve(input$faas_ld_region)
          },
          "OpenWhisk"={
-           json$ComputeServers[[faas_name]]$Endpoint <- input$faas_ow_end
-           json$ComputeServers[[faas_name]]$Namespace <- input$faas_ow_name
-           json$ComputeServers[[faas_name]]$Region <- input$faas_ow_region
+           json$ComputeServers[[faas_name]]$Endpoint <- retrieve(input$faas_ow_end)
+           json$ComputeServers[[faas_name]]$Namespace <- retrieve(input$faas_ow_name)
+           json$ComputeServers[[faas_name]]$Region <- retrieve(input$faas_ow_region)
          }
   )
   json_source <- jsonlite::toJSON(json, auto_unbox=TRUE)
@@ -52,10 +52,10 @@ ob_data_apply <- function(input, output, session, json){
   if (is.null(data_name)){
     return()
   }
-  json$DataStores[[data_name]]$Endpoint <- input$data_endpoint
-  json$DataStores[[data_name]]$Bucket <- input$data_bucket
-  json$DataStores[[data_name]]$Region <- input$data_region
-  json$DataStores[[data_name]]$Writable <- input$data_writable
+  json$DataStores[[data_name]]$Endpoint <- retrieve(input$data_endpoint)
+  json$DataStores[[data_name]]$Bucket <- retrieve(input$data_bucket)
+  json$DataStores[[data_name]]$Region <- retrieve(input$data_region)
+  json$DataStores[[data_name]]$Writable <- retrieve(input$data_writable)
   json_source <- jsonlite::toJSON(json, auto_unbox=TRUE)
   json_pretty <- jsonlite::prettify(json_source)
   
@@ -64,15 +64,15 @@ ob_data_apply <- function(input, output, session, json){
 
 
 ob_gen_apply <- function(input, output, session, json){
-  json$FunctionInvoke <- input$function_invoke
-  json$InvocationID <- input$invocation_id
-  json$FaaSrLog <- input$faasr_log
+  json$FunctionInvoke <- retrieve(input$function_invoke)
+  json$InvocationID <- retrieve(input$invocation_id)
+  json$FaaSrLog <- retrieve(input$faasr_log)
   if (input$logging_data_server == "None"){
     json$LoggingDataStore <- NULL
   } else {
-    json$LoggingDataStore <- input$logging_data_server
+    json$LoggingDataStore <- retrieve(input$logging_data_server)
   }
-  json$DefaultDataStore <- input$default_data_server
+  json$DefaultDataStore <- retrieve(input$default_data_server)
   json_source <- jsonlite::toJSON(json, auto_unbox=TRUE)
   json_pretty <- jsonlite::prettify(json_source)
   
